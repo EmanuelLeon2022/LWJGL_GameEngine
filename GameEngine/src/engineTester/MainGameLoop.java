403c22a8a70eb70b51ee5fc906ef40326328387e
Emanuel Leon-Vargas <146137985+EmanuelLeon2022@users.noreply.github.com>
2025-12-27 21:26:46 -0500

    Updated Terrain method

--- GameEngine/src/engineTester/MainGameLoop.java
+++ GameEngine/src/engineTester/MainGameLoop.java
@@ -1,61 +1,78 @@
 package engineTester;
 
-import models.RawModel;
-import models.TexturedModel;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
+
+import models.RawModel;
+import models.TexturedModel;
+
+
 import org.lwjgl.opengl.Display;
 import org.lwjgl.util.vector.Vector3f;
+
 
 import renderEngine.DisplayManager;
 import renderEngine.Loader;
 import renderEngine.MasterRenderer;
 import renderEngine.OBJLoader;
-import renderEngine.Renderer;
-import shaders.StaticShader;
+import terrains.Terrain;
 import textures.ModelTexture;
 import entities.Camera;
 import entities.Entity;
 import entities.Light;
 
+
 public class MainGameLoop {
+
 
 	public static void main(String[] args) {
 
+
 		DisplayManager.createDisplay();
 		Loader loader = new Loader();
-
 		
 		
-		RawModel model = OBJLoader.loadObjModel("dragon", loader);
-		TexturedModel staticModel = new TexturedModel(model,new ModelTexture(
-				loader.loadTexture("white")));
+		RawModel model = OBJLoader.loadObjModel("tree", loader);
 		
-		Entity entity = new Entity(staticModel, new Vector3f(0,0,-50),0,0,0,1);
-		Light light  = new Light (new Vector3f(3000,2000,3000),new Vector3f(1,1,1));
+		TexturedModel staticModel = new TexturedModel(model,new ModelTexture(loader.loadTexture("tree")));
 		
-		Camera camera = new Camera();
+		List<Entity> entities = new ArrayList<Entity>();
+		Random random = new Random();
+		for(int i=0;i<500;i++){
+			entities.add(new Entity(staticModel, new Vector3f(random.nextFloat()*800 - 400,0,random.nextFloat() * -600),0,0,0,3));
+		}
 		
+		Light light = new Light(new Vector3f(20000,20000,2000),new Vector3f(1,1,1));
 		
+		Terrain terrain = new Terrain(0,0,loader,new ModelTexture(loader.loadTexture("grass")));
+		Terrain terrain2 = new Terrain(1,0,loader,new ModelTexture(loader.loadTexture("grass")));
+		
+		Camera camera = new Camera();	
 		MasterRenderer renderer = new MasterRenderer();
+		
 		while(!Display.isCloseRequested()){
 			camera.move();
 			
-//			 for (Entity cube: allCubes){
-//			 renderer.processEntity(cube);
-//			 }
-			 
+			renderer.processTerrain(terrain);
+			renderer.processTerrain(terrain2);
+			for(Entity entity:entities){
+				renderer.processEntity(entity);
+			}
 			renderer.render(light, camera);
 			DisplayManager.updateDisplay();
 		}
-		
+
+
 		renderer.cleanup();
 		loader.cleanUp();
 		DisplayManager.closeDisplay();
 
+
 	}
 
+
 }
+
